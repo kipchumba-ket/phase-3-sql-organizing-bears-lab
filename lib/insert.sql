9a8b478f967edb8f5dc0c5c0c1d99cf5c4f49538
kipchumba-ket <kipchumbaelly97@gmail.com>
2023-02-16 11:34:03 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+-- INSERT INTO bears VALUES (
+--     (1,"Stl", 3, "M", "red", "hot", 1),
+--     (1,"Stl", 3, "M", "red", "hot", 0),
+--     (1,"Stl", 3, "F", "red", "hot", 0),
+--     (1,"Stl", 3, "F", "red", "hot", 0),
+--     (1,"Stl", 3, "F", "red", "hot", 0),
+--     (1,"Stl", 3, "F", "red", "hot", 1),
+--     (1,"Stl", 3, "M", "red", "hot", 1),
+--     (1,null, 3, "F", "red white", "hot", 0)
+-- );
+
+INSERT INTO bears VALUES (1,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (2,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (3,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (4,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (5,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (6,null, 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (7,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (8,"red", 1, "F", "purple", "hot", 1);
